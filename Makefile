3ffc19825828a884f72d077ebfde0ddc92df2a12
Emanuele Salandin <esalandin@alice.it>
2014-06-12 03:47:36 +0200

    detects payload

--- Makefile
+++ Makefile
@@ -1,4 +1,13 @@
 LDFLAGS = -L ../libpcap-1.5.3
 LDLIBS = -lpcap.1.5.3
+CXXFLAGS = -g
+
+.PHONY: all test
 
 all: pcap-read
+
+test: pcap-read
+	./pcap-read captures/verizon-11062014.pcap
+
+clean:
+	rm -f pcap-read
--- pcap-read.cc
+++ pcap-read.cc
@@ -7,15 +7,25 @@
 //defines for the packet type code in an ETHERNET header
 #define ETHER_TYPE_IP (0x0800)
 #define ETHER_TYPE_8021Q (0x8100)
-
 //------------------------------------------------------------------- 
+
+void dump_iphdr(const struct ip *ip) {
+	fprintf(stderr, "ip_len %u; ", ntohs(ip->ip_len));
+	fprintf(stderr, "ip_src %s; ", inet_ntoa(ip->ip_src));
+	fprintf(stderr, "ip_dst %s; ", inet_ntoa(ip->ip_dst));
+	if (ip->ip_p == 6) {
+		fprintf(stderr, "ip_p TCP;");
+	}
+	else {
+		fprintf(stderr, "ip_p %u; ", ip->ip_p);
+	}
+	fprintf(stderr, "\n");
+}
+
+//-------------------------------------------------------------------
 int main(int argc, char **argv) 
 { 
   unsigned int pkt_counter=0;   // packet counter 
-  unsigned long byte_counter=0; //total bytes seen in entire trace 
-  unsigned long cur_counter=0; //counter for current 1-second interval 
-  unsigned long max_volume = 0;  //max value of bytes in one-second interval 
-  unsigned long current_ts=0; //current timestamp 
  
   //temporary packet buffers 
   struct pcap_pkthdr header; // The header that pcap gives us 
@@ -23,29 +33,27 @@ int main(int argc, char **argv)
   
   //check command line arguments 
   if (argc < 2) { 
-    fprintf(stderr, "Usage: %s [input pcaps]\n", argv[0]); 
+    fprintf(stderr, "Usage: %s [input pcap]\n", argv[0]);
     exit(1); 
   } 
   
-  //-------- Begin Main Packet Processing Loop ------------------- 
-  //loop through each pcap file in command line args 
-  for (int fnum=1; fnum < argc; fnum++) {  
- 
+  const char *fname= argv[1];
+
     //----------------- 
     //open the pcap file 
     pcap_t *handle; 
     char errbuf[PCAP_ERRBUF_SIZE]; //not sure what to do with this, oh well 
-    handle = pcap_open_offline(argv[fnum], errbuf);   //call pcap library function 
+    handle = pcap_open_offline(fname, errbuf);   //call pcap library function
  
     if (handle == NULL) { 
-      fprintf(stderr,"Couldn't open pcap file %s: %s\n", argv[fnum], errbuf); 
+      fprintf(stderr,"Couldn't open pcap file %s: %s\n", fname, errbuf);
       return(2); 
     } 
  
     //----------------- 
     //begin processing the packets in this particular file, one at a time 
  
-    while (packet = pcap_next(handle,&header)) { 
+    while ((packet = pcap_next(handle,&header))!=0) {
       // header contains information about the packet (e.g. timestamp) 
       u_char *pkt_ptr = (u_char *)packet; //cast a pointer to the packet data 
       
@@ -55,41 +63,52 @@ int main(int argc, char **argv)
  
       if (ether_type == ETHER_TYPE_IP) //most common 
         ether_offset = 14; 
-      else if (ether_type == ETHER_TYPE_8021Q) //my traces have this 
-         ether_offset = 18; 
-      else 
+      else {
          fprintf(stderr, "Unknown ethernet type, %04X, skipping...\n", ether_type); 
- 
+         continue;
+      }
+
       //parse the IP header 
       pkt_ptr += ether_offset;  //skip past the Ethernet II header 
       struct ip *ip_hdr = (struct ip *)pkt_ptr; //point to an IP header structure 
- 
+      // dump_iphdr(ip_hdr);
       int packet_length = ntohs(ip_hdr->ip_len); 
  
-      //check to see if the next second has started, for statistics purposes 
-      if (current_ts == 0) {  //this takes care of the very first packet seen 
-         current_ts = header.ts.tv_sec; 
-      } else if (header.ts.tv_sec > current_ts) { 
-         printf("%d KBps\n", cur_counter/1000); //print 
-         cur_counter = 0; //reset counters 
-         current_ts = header.ts.tv_sec; //update time interval 
-      } 
- 
-      cur_counter += packet_length; 
-      byte_counter += packet_length; //byte counter update 
+      uint ip_header_len= ip_hdr->ip_hl*4;
+      pkt_ptr += ip_header_len;
+
+      if (ip_hdr->ip_p != 6) {
+    	  fprintf(stderr, "Unknown ip protocol %u\n", ip_hdr->ip_p);
+    	  continue;
+      }
+
+      u_char *tcp_hdr= pkt_ptr;
+
+      uint16_t src_port= (tcp_hdr[0]<<8) + tcp_hdr[1];
+      uint16_t dst_port= (tcp_hdr[2]<<8) + tcp_hdr[3];
+      uint32_t seq_num= (tcp_hdr[4]<<24) + (tcp_hdr[5]<<16) + (tcp_hdr[6]<<8) + tcp_hdr[7];
+      uint8_t data_offset= tcp_hdr[12]>>4;
+      //... parse needed options....
+      uint tcp_header_len= data_offset*4;
+
+      pkt_ptr += tcp_header_len;
+      uint payload_size= packet_length - ip_header_len - tcp_header_len;
+      // here data starts....
+      printf("packet %u; ", pkt_counter+1);
+      printf("len= %u; ", payload_size);
+      for (uint i=0; i<payload_size && i<32; ++i) {
+    	  printf("%02X ", pkt_ptr[i]);
+      }
+      printf("\n");
+
       pkt_counter++; //increment number of packets seen 
  
     } //end internal loop for reading packets (all in one file) 
  
     pcap_close(handle);  //close the pcap file 
  
-  } //end for loop through each command line argument 
   //---------- Done with Main Packet Processing Loop --------------  
  
-  //output some statistics about the whole trace 
-  byte_counter /= 1e6;  //convert to MB to make easier to read 
- 
-  printf("Processed %d packets and %u MBytes, in %d files\n", pkt_counter, byte_counter, argc-1);
   return 0; //done
 } //end of main() function
 
